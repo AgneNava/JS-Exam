2a1a779b4f11576581a0ad9ef8f4b5affa3e41d9
AgneNava <sak.agne@gmail.com>
2022-05-18 15:12:49 +0300

    DOM elements selected

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,6 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+const weightConverter = document.querySelector("form");
+const kilogramsInput = document.getElementById("search");
+const outputDiv = document.getElementById("output");
