29fdadf4092f9e50be9644441f5dddb7a52db664
AgneNava <sak.agne@gmail.com>
2022-05-18 15:22:59 +0300

    Click counter code

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,13 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+let clickMe = document.getElementById("btn__element");
+var countClicks = 0;
+
+clickMe.addEventListener("click", function () {
+  countClicks += 1;
+  console.log(countClicks);
+
+  document.getElementById("btn__state").innerHTML = countClicks;
+});
