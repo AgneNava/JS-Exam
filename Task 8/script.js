e9aafdce8e52e06aac06474974b2ab92f4e1fe67
AgneNava <sak.agne@gmail.com>
2022-05-18 15:48:17 +0300

    Calculator code

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,13 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(a, b) {
+  this.sum = a + b;
+  this.subtraction = a - b;
+  this.multiplication = a * b;
+  this.division = a / b;
+  console.log(this.sum, this.subtraction, this.multiplication, this.division);
+}
+
+const object = new Calculator(10, 4);
